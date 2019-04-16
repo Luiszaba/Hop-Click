40f58eb74ff2a53630e5276dcf733a0e4371f55a
Luis Zabala <luis.zabala076@gmail.com>
2019-04-16 19:30:58 -0400

    login complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
   root "application#welcome"
 
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 
+  devise_scope :user do
+    get 'sign_up', to: 'devise/registrations#new'
+  end
 end
