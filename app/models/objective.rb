280cc03a4536c448e11c2b5126cb24f36c1510a2
Luis Zabala <luis.zabala076@gmail.com>
2019-04-17 12:38:41 -0400

    comment for Objective model

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,6 +1,8 @@
 class Objective < ApplicationRecord
 
   belongs_to :group, optional: true
+  # set belongs_to :group, optional :true,
+  # => having difficulties associating Group user_id and User user_id with Objective
   belongs_to :user
   accepts_nested_attributes_for :group
 
