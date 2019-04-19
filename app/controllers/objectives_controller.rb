8b06f60f8505097cb1daf122b39e0627fcdc8862
Luis Zabala <luis.zabala076@gmail.com>
2019-04-19 09:01:22 -0400

    simplified objective controller

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -9,29 +9,24 @@ class ObjectivesController < ApplicationController
 
   def show; end
 
-  def new
-    @objective = Objective.new
-    @groups = Group.where('id = ?', current_user.group_id)
-  end
-
-
   def edit; end
 
-  def create
-      @objective = current_user.objectives.build(objective_params)
-      respond_to do |format|
-        if @objective.save
-
-          format.html {redirect_to @objective, notice: 'Objective was successfully created.'}
-          format.json {render :show, status: :created, location: @objective}
-        else
-          format.html {render :new}
-          format.json {render json: @objective.errors, status: :unprocessable_entity}
-        end
-      end
+  def new
+    @objective = Objective.new
   end
 
-
+  def create
+    @objective = Objective.new(objective_params)
+    respond_to do |format|
+      if @objective.save
+        format.html {redirect_to @objective, notice: 'Objective was successfully created.'}
+        format.json {render :show, status: :created, location: @objective}
+      else
+        format.html {render :new}
+        format.json {render json: @objective.errors, status: :unprocessable_entity}
+      end
+    end
+  end
 
   def update
     respond_to do |format|
@@ -45,28 +40,22 @@ class ObjectivesController < ApplicationController
     end
   end
 
-
   def destroy
-    if @objective.user == current_user
-      @objective.destroy
-      respond_to do |format|
-        format.html {redirect_to objectives_url, notice: 'Objective was successfully destroyed.'}
-        format.json {head :no_content}
-      end
+    @objective.destroy
+    respond_to do |format|
+      format.html {redirect_to objectives_url, notice: 'Objective was successfully destroyed.'}
+      format.json {head :no_content}
     end
-  end
-
-  private
-
-
-  def set_objective
-    @objective = Objective.find(params[:id])
-  end
-
-  def objective_params
-    params.require(:objective).permit(:name, :description, :group_id)
   end
 end
 
+private
 
+def set_objective
+  @objective = Objective.find(params[:id])
+end
 
+def objective_params
+  params.require(:objective).permit(:name, :description, :group_id,
+                                    :due_date, :user_id)
+end
